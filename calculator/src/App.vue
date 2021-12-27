556daff6af9e1d3d5ded97949ce1c3bc4328ee20
Erik Augusto Lima Evangelista <eriklim@hotmail.com>
2021-12-27 11:01:08 -0300

    primeiro-commit-projeto-calculadora-vuejs-27-12-2021

---
+++ calculator/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+ <div id="app">
+     <h1>Olá Vue!</h1>
+ </div> 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+    @font-face{
+
+        font-family: "RobotoMono";
+        src: url( "./fonts/RobotoMono-Thin.ttf" );
+
+    }
+
+    * {
+        
+        font-family: "RobotoMono", monospace;
+
+    }
+
+    body {
+
+        margin: 0;
+
+    }
+
+    #app {
+
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-content: center;
+        text-align: center;
+        color: #fff;
+        background: linear-gradient( to right, rgb(73, 1, 12), rgb(45, 10, 46) )
+    }
+
+</style>
---
+++ calculator/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+
+new Vue({
+
+    render: h => h( App )
+
+    // render( createElement ) {
+
+    //     return createElement( App )
+
+    // }
+
+}).$mount( '#app' )
